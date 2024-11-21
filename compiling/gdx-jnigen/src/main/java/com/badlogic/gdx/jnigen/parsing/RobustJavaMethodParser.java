e7973a3b95d854578024159c74e6e6a8f3bfad9c
Mohammad Alkhatib <mohammad.alkhatib@web.de>
2024-11-21 12:27:42 +0100

    Changed log message.

--- compiling/gdx-jnigen/src/main/java/com/badlogic/gdx/jnigen/parsing/RobustJavaMethodParser.java
+++ compiling/gdx-jnigen/src/main/java/com/badlogic/gdx/jnigen/parsing/RobustJavaMethodParser.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2011 See AUTHORS file.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,30 +18,20 @@ package com.badlogic.gdx.jnigen.parsing;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.body.BodyDeclaration;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.EnumDeclaration;
-import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.body.Parameter;
-import com.github.javaparser.ast.body.TypeDeclaration;
+import com.github.javaparser.ast.body.*;
 
 import java.io.ByteArrayInputStream;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Stack;
+import java.util.*;
 
 public class RobustJavaMethodParser implements JavaMethodParser {
+	
 	private static final String JNI_MANUAL = "MANUAL";
 	private static final Map<String, ArgumentType> plainOldDataTypes;
 	private static final Map<String, ArgumentType> arrayTypes;
 	private static final Map<String, ArgumentType> bufferTypes;
 	private static final Map<String, ArgumentType> otherTypes;
 	public static String CustomIgnoreTag = "";
-
+	
 	static {
 		plainOldDataTypes = new HashMap<String, ArgumentType>();
 		plainOldDataTypes.put("boolean", ArgumentType.Boolean);
@@ -52,7 +42,7 @@ public class RobustJavaMethodParser implements JavaMethodParser {
 		plainOldDataTypes.put("long", ArgumentType.Long);
 		plainOldDataTypes.put("float", ArgumentType.Float);
 		plainOldDataTypes.put("double", ArgumentType.Double);
-
+		
 		arrayTypes = new HashMap<String, ArgumentType>();
 		arrayTypes.put("boolean", ArgumentType.BooleanArray);
 		arrayTypes.put("byte", ArgumentType.ByteArray);
@@ -62,7 +52,7 @@ public class RobustJavaMethodParser implements JavaMethodParser {
 		arrayTypes.put("long", ArgumentType.LongArray);
 		arrayTypes.put("float", ArgumentType.FloatArray);
 		arrayTypes.put("double", ArgumentType.DoubleArray);
-
+		
 		bufferTypes = new HashMap<String, ArgumentType>();
 		bufferTypes.put("Buffer", ArgumentType.Buffer);
 		bufferTypes.put("ByteBuffer", ArgumentType.ByteBuffer);
@@ -78,11 +68,11 @@ public class RobustJavaMethodParser implements JavaMethodParser {
 		otherTypes.put("Class", ArgumentType.Class);
 		otherTypes.put("Throwable", ArgumentType.Throwable);
 	}
-
+	
 	Stack<TypeDeclaration<?>> classStack = new Stack<TypeDeclaration<?>>();
-
+	
 	@Override
-	public ArrayList<JavaSegment> parse (String classFile) throws Exception {
+	public ArrayList<JavaSegment> parse(String classFile) throws Exception {
 		CompilationUnit unit = new JavaParser().parse(new ByteArrayInputStream(classFile.getBytes())).getResult().get();
 		ArrayList<JavaMethod> methods = new ArrayList<JavaMethod>();
 		getJavaMethods(methods, getOuterClass(unit));
@@ -92,21 +82,21 @@ public class RobustJavaMethodParser implements JavaMethodParser {
 		ArrayList<JavaSegment> segments = sortMethodsAndSections(methods, sections);
 		return segments;
 	}
-
-	private ArrayList<JavaSegment> sortMethodsAndSections (ArrayList<JavaMethod> methods, ArrayList<JniSection> sections) {
+	
+	private ArrayList<JavaSegment> sortMethodsAndSections(ArrayList<JavaMethod> methods, ArrayList<JniSection> sections) {
 		ArrayList<JavaSegment> segments = new ArrayList<JavaSegment>();
 		segments.addAll(methods);
 		segments.addAll(sections);
 		Collections.sort(segments, new Comparator<JavaSegment>() {
 			@Override
-			public int compare (JavaSegment o1, JavaSegment o2) {
+			public int compare(JavaSegment o1, JavaSegment o2) {
 				return o1.getStartIndex() - o2.getStartIndex();
 			}
 		});
 		return segments;
 	}
-
-	private void alignMethodBodies (ArrayList<JavaMethod> methods, ArrayList<JniSection> methodBodies) {
+	
+	private void alignMethodBodies(ArrayList<JavaMethod> methods, ArrayList<JniSection> methodBodies) {
 		for (JavaMethod method : methods) {
 			for (JniSection section : methodBodies) {
 				if (method.getEndIndex() == section.getStartIndex()) {
@@ -120,17 +110,18 @@ public class RobustJavaMethodParser implements JavaMethodParser {
 			}
 		}
 	}
-
-	private void getJavaMethods (ArrayList<JavaMethod> methods, TypeDeclaration<?> type) {
+	
+	private void getJavaMethods(ArrayList<JavaMethod> methods, TypeDeclaration<?> type) {
 		classStack.push(type);
 		if (type.getMembers() != null) {
 			for (BodyDeclaration<?> member : type.getMembers()) {
 				if (member.isClassOrInterfaceDeclaration() || member.isEnumDeclaration()) {
-					getJavaMethods(methods, (TypeDeclaration<?>)member);
+					getJavaMethods(methods, (TypeDeclaration<?>) member);
 				} else {
 					if (member instanceof MethodDeclaration) {
-						MethodDeclaration method = (MethodDeclaration)member;
-						if(!method.isNative()) continue;
+						MethodDeclaration method = (MethodDeclaration) member;
+						if (!method.isNative())
+							continue;
 						methods.add(createMethod(method));
 					}
 				}
@@ -138,55 +129,60 @@ public class RobustJavaMethodParser implements JavaMethodParser {
 		}
 		classStack.pop();
 	}
-
-	private JavaMethod createMethod (MethodDeclaration method) {
+	
+	private JavaMethod createMethod(MethodDeclaration method) {
 		String className = classStack.peek().getNameAsString();
 		String name = method.getNameAsString();
 		boolean isStatic = method.isStatic();
 		String returnType = method.getType().toString();
 		ArrayList<Argument> arguments = new ArrayList<Argument>();
-
+		
 		if (method.getParameters() != null) {
 			for (Parameter parameter : method.getParameters()) {
 				arguments.add(new Argument(getArgumentType(parameter), parameter.getNameAsString()));
 			}
 		}
-
+		
 		return new JavaMethod(className, name, isStatic, returnType, null, arguments, method.getBegin().get().line, method.getEnd().get().line);
 	}
-
-	private ArgumentType getArgumentType (Parameter parameter) {
+	
+	private ArgumentType getArgumentType(Parameter parameter) {
 		String[] typeTokens = parameter.getType().toString().split("\\.");
 		String type = typeTokens[typeTokens.length - 1];
 		int arrayDim = 0;
 		for (int i = 0; i < type.length(); i++) {
-			if (type.charAt(i) == '[') arrayDim++;
+			if (type.charAt(i) == '[')
+				arrayDim++;
 		}
 		type = type.replace("[", "").replace("]", "");
-
+		
 		if (arrayDim >= 1) {
-			if (arrayDim > 1) return ArgumentType.ObjectArray;
+			if (arrayDim > 1)
+				return ArgumentType.ObjectArray;
 			ArgumentType arrayType = arrayTypes.get(type);
 			if (arrayType == null) {
 				return ArgumentType.ObjectArray;
 			}
 			return arrayType;
 		}
-
-		if (plainOldDataTypes.containsKey(type)) return plainOldDataTypes.get(type);
-		if (bufferTypes.containsKey(type)) return bufferTypes.get(type);
-		if (otherTypes.containsKey(type)) return otherTypes.get(type);
+		
+		if (plainOldDataTypes.containsKey(type))
+			return plainOldDataTypes.get(type);
+		if (bufferTypes.containsKey(type))
+			return bufferTypes.get(type);
+		if (otherTypes.containsKey(type))
+			return otherTypes.get(type);
 		return ArgumentType.Object;
 	}
-
-	private TypeDeclaration<?> getOuterClass (CompilationUnit unit) {
-		for (TypeDeclaration<?> type : unit.getTypes()) {
-			if (type instanceof ClassOrInterfaceDeclaration || type instanceof EnumDeclaration) return type;
-		}
-		throw new RuntimeException("Couldn't find class, is your java file empty? Unit: " + unit);
+	
+	private TypeDeclaration<?> getOuterClass(CompilationUnit unit) {
+		for (TypeDeclaration<?> type : unit.getTypes())
+			if (type instanceof ClassOrInterfaceDeclaration || type instanceof EnumDeclaration)
+				return type;
+		throw new RuntimeException("Unable to find class, unit: " + unit);
 	}
-
-	private ArrayList<JniSection> getJniSections (String classFile) {
+	
+	private ArrayList<JniSection> getJniSections(String classFile) {
 		ArrayList<JniSection> sections = getComments(classFile);
 		Iterator<JniSection> iter = sections.iterator();
 		while (iter.hasNext()) {
@@ -199,22 +195,25 @@ public class RobustJavaMethodParser implements JavaMethodParser {
 		}
 		return sections;
 	}
-
-	private ArrayList<JniSection> getNativeCodeBodies (String classFile) {
+	
+	private ArrayList<JniSection> getNativeCodeBodies(String classFile) {
 		ArrayList<JniSection> sections = getComments(classFile);
 		Iterator<JniSection> iter = sections.iterator();
 		while (iter.hasNext()) {
 			JniSection section = iter.next();
-			if (section.getNativeCode().startsWith("JNI")) iter.remove();
-			if (section.getNativeCode().startsWith("-{")) iter.remove();
-			if (!CustomIgnoreTag.isEmpty() && section.getNativeCode().startsWith(CustomIgnoreTag)) iter.remove();
+			if (section.getNativeCode().startsWith("JNI"))
+				iter.remove();
+			if (section.getNativeCode().startsWith("-{"))
+				iter.remove();
+			if (!CustomIgnoreTag.isEmpty() && section.getNativeCode().startsWith(CustomIgnoreTag))
+				iter.remove();
 		}
 		return sections;
 	}
-
-	private ArrayList<JniSection> getComments (String classFile) {
+	
+	private ArrayList<JniSection> getComments(String classFile) {
 		ArrayList<JniSection> sections = new ArrayList<JniSection>();
-
+		
 		boolean inComment = false;
 		int start = 0;
 		int startLine = 0;
@@ -223,7 +222,8 @@ public class RobustJavaMethodParser implements JavaMethodParser {
 			char c1 = classFile.charAt(i);
 			char c2 = classFile.charAt(i + 1);
 			char c3 = classFile.charAt(i + 2);
-			if (c1 == '\n') line++;
+			if (c1 == '\n')
+				line++;
 			if (!inComment) {
 				if (c1 == '/' && c2 == '*' && c3 != '*') {
 					inComment = true;
@@ -237,7 +237,8 @@ public class RobustJavaMethodParser implements JavaMethodParser {
 				}
 			}
 		}
-
+		
 		return sections;
 	}
+	
 }
